ab19d9a7f43e8d528d96155768102d06550b25b2
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2021-06-23 14:51:10 -0400

    Create Auth Functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -31,4 +31,26 @@ export class AuthService {
     );
   }
 
+  async googleSignin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  private updateUserData(user) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    const data = { 
+      uid: user.uid, 
+      email: user.email, 
+      displayName: user.displayName, 
+      photoURL: user.photoURL
+    }
+    return userRef.set(data, { merge: true })
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
 }
